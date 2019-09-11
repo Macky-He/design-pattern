8d3b3c44fe347149e72f9cd074eef7a0543e3986
Macky-He <97462319@qq.com>
2019-09-11 19:06:52 +0800

    新增备忘录模式

--- src/main/java/com/macky/designpattern/mementopattern/MementoCaretaker.java
+++ src/main/java/com/macky/designpattern/mementopattern/MementoCaretaker.java
@@ -1,5 +1,6 @@
 package com.macky.designpattern.mementopattern;
 import	java.security.Provider.Service;
+import java.util.ArrayList;
 
 import lombok.Data;
 import lombok.Getter;
@@ -11,8 +12,16 @@ import lombok.Setter;
  * @Description: 负责人：象棋棋子备忘录管理类
  * @date 2019/9/11 17:07
  */
-@Getter
-@Setter
+
 public class MementoCaretaker {
-    private ChessmanMemento memento;
+    //定义一个集合来存储多个备忘录
+    private ArrayList mementolist = new ArrayList();
+
+    public ChessmanMemento getMemento(int i) {
+        return (ChessmanMemento)mementolist.get(i);
+    }
+
+    public void setMemento(ChessmanMemento memento) {
+        mementolist.add(memento);
+    }
 }
